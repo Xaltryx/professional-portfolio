78849c5e4fdb24ff0b4f71ea18fcab0bfeacd512
Youssef Sayed <y.newoten@gmail.com>
2025-12-05 10:07:25 +0200

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-hours = int(input("Enter hours worked: "))
-rate = int(input("Enter hourly rate: "))
-total = hours*rate
-print("Total pay:",total)
-print("Type of hours:",type(hours))
-print("Type of rate:",type(rate))
-print("Type of total pay:",type(total))
