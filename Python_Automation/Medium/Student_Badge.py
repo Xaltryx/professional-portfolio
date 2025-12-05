0e52d2b1cae07c80d80714ef2983eb4df788ebb4
Youssef Sayed <y.newoten@gmail.com>
2025-12-05 10:07:13 +0200

    Delete Python_Automation/Medium/Student_Badge.py

--- Python_Automation/Medium/Student_Badge.py
+++
@@ -1,7 +0,0 @@
-name = input("Enter student name: ")
-grade = int(input("Enter grade level: "))
-if grade <= 0:
-	print("Invalid grade level. Cannot create badge.")
-else:
-	badge = name[0] * (grade-1) + name
-	print(badge)
