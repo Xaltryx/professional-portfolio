4ceb24ccb6ff073c99c7593da6641459fcae3c55
Youssef Sayed <y.newoten@gmail.com>
2025-12-07 16:23:46 +0200

    Update main.py

--- Python_Automation/Easy/Ticket_Validation/main.py
+++ Python_Automation/Easy/Ticket_Validation/main.py
@@ -1,5 +1,8 @@
-ticket = int(input("Enter ticket number: "))
-if ticket > 0:
-	print("Valid ticket")
-else:
-	print("Invalid ticket")
+try:
+	ticket = int(input("Enter ticket number: "))
+	if ticket > 0:
+		print("Valid ticket")
+	else:
+		print("Invalid ticket")
+except ValueError:
+	print("Please write a number.")
